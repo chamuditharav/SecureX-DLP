df4c2e82e3415fd4e97ced76b571b51dc1fd6f2d
chamuditharav <chamuditharavindu.macr@gmail.com>
2022-10-16 17:52:35 +0530

    SVR and Client Error Fixed

--- client.py
+++ client.py
@@ -5,12 +5,29 @@ import threading
 import rsa
 from cryptography.fernet import Fernet
 
+
 clientID = "Client1"
 keys = {"priK":"", "pubK":"", "shareKey":""}
 
 # Connecting To Server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 65432))
+
+def con():
+    global client
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 65432))
+
+
+def MSGencrypt(data):
+    fernet = keys["shareKey"]
+    enc = fernet.encrypt(data.encode())
+    return(enc)
+    
+
+
+def MSGdecrypt(data):
+    fernet = keys["shareKey"]
+    dec = fernet.decrypt(bytes.fromhex(data))
+    return(dec.decode())
 
 
 def generateKeys(ksize=1024):
@@ -27,7 +44,13 @@ def decrypt(ciphertext, key):
     except:
         print("Decryption Error!")
 
+def saveKey(key):
+    keys["shareKey"] = Fernet(key)
+    #print(keys)
+    print("Key Shared!")
+
 def keyEx():
+    print("Initializing key exchange.....")
     publicKey, privateKey = generateKeys()
     keys["priK"] = privateKey
     keys["pubK"] = publicKey
@@ -37,33 +60,67 @@ def keyEx():
 def receive():
     while True:
         try:
-            DataFrame = client.recv(4096).decode('ascii')
-            if (f"{clientID}:SVR-KEYEX-RPLY" in DataFrame):
-                DataFrame = DataFrame.split(":")
-                #print(DataFrame)
-                keys["shareKey"] = decrypt(bytes.fromhex(DataFrame[2]),keys["priK"])
-                print(keys["shareKey"])
+            dataFrame = client.recv(1024).decode('ascii')
+            if (f"{clientID}:SVR-KEYEX-RPLY" in dataFrame):
+                dataFrame = dataFrame.split(":")
+                #print(dataFrame)
+                
+                #saveKey(decrypt(bytes.fromhex(dataFrame[2]),keys["priK"]))
+                print("Key recieved!")
+                print("Decrypting the key.......")
+                encKey = decrypt(bytes.fromhex(dataFrame[2]),keys["priK"])
+                print("Key decrypted!")
+                print(encKey)
+                keySave_thread = threading.Thread(target=saveKey, args=(encKey.encode(),))
+                keySave_thread.start()
+                
+            elif (f"{clientID}:ENC-MSG" in dataFrame):
+                dataFrame = dataFrame.split(":")
+                print(f"{clientID} <-- SVR : {MSGdecrypt(dataFrame[2])}")
+                print("\n")
+
             else:
-                print(DataFrame)
+                print(dataFrame)
         except:
-            print("An error occured!")
+            print("An Error Occured!")
             client.close()
+            #con()
+            #init_()
             break
 
 def send(message):
     client.send(bytes(f"{clientID}:{message}", 'ascii'))
+
+def sendEnc(message):
+    #print(MSGencrypt(message).hex())
+    send(f"ENC-MSG:{MSGencrypt(message).hex()}")
+    #client.send(bytes(f"{clientID}:{message}", 'ascii'))
 
 
 def mainLoop():
     keyEx()
     while True:
         if(not keys["shareKey"] == ""):
-            send(str(input("> ")))
+
+            terminal = str(input("> "))
+            sendEnc(terminal)
+            print(f"{clientID} --> SVR : {terminal}")
+            
 
 
+def init_():
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
 
-write_thread = threading.Thread(target=mainLoop)
-write_thread.start()
+    write_thread = threading.Thread(target=mainLoop)
+    write_thread.start()
+
+
+if __name__ == "__main__":
+
+    try:
+        con()
+        init_()
+    except:
+        print("An Error Occured!")
